c81240ad2095b0cb056fa11d0722c06a21761cd6
Juan Elias <juan.elias1194@gmail.com>
2024-09-03 11:38:33 -0600

    Agregar archivos de proyecto.

---
+++ balanza uart/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.lblPortName = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox1.Location = New System.Drawing.Point(33, 52)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(579, 31)
+        Me.TextBox1.TabIndex = 0
+        Me.TextBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right
+        '
+        'lblPortName
+        '
+        Me.lblPortName.AutoSize = True
+        Me.lblPortName.Location = New System.Drawing.Point(514, 9)
+        Me.lblPortName.Name = "lblPortName"
+        Me.lblPortName.Size = New System.Drawing.Size(39, 13)
+        Me.lblPortName.TabIndex = 1
+        Me.lblPortName.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(660, 153)
+        Me.Controls.Add(Me.lblPortName)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents lblPortName As Label
+End Class
---
+++ balanza uart/Form1.vb
@@ -0,0 +1,48 @@
+﻿Imports System.IO.Ports
+Public Class Form1
+    Private WithEvents serialPort As New SerialPort()
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'serialPort.PortName = "COM4"
+        serialPort.BaudRate = 9600
+        serialPort.Parity = Parity.None
+        serialPort.DataBits = 8
+        serialPort.StopBits = StopBits.One
+
+        Try
+            ' Obtén la lista de puertos serie disponibles
+            Dim availablePorts() As String = SerialPort.GetPortNames()
+
+            ' Verifica si hay al menos un puerto disponible
+            If availablePorts.Length > 0 Then
+                ' Usa el primer puerto disponible
+                serialPort.PortName = availablePorts(0)
+                ' Abre la conexión serie
+                serialPort.Open()
+
+                lblPortName.Text = "Puerto Serie: " & serialPort.PortName
+            Else
+                MessageBox.Show("No se encontraron puertos serie disponibles.")
+            End If
+        Catch ex As Exception
+            MessageBox.Show("Error al abrir el puerto serie: " & ex.Message)
+        End Try
+    End Sub
+
+    Private Sub SerialPort_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles serialPort.DataReceived
+        Dim data As String = serialPort.ReadExisting()
+
+        If TextBox1.InvokeRequired Then
+            TextBox1.Invoke(Sub() TextBox1.Text = "Datos recibidos: " & data)
+        Else
+            TextBox1.Text = "Datos recibidos: " & data
+        End If
+
+        Console.WriteLine("Datos recibidos: " & data)
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        If serialPort.IsOpen Then
+            serialPort.Close()
+        End If
+    End Sub
+End Class
